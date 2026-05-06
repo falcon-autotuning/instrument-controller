b706712589d477b33e5a8ba090943157c8790cc3
tjk144 <tkovach@wisc.edu>
2026-05-06 00:52:42 -0500

    Updated the ports ofr the latest instrument script server installation

--- ports/instrument-script-server/portfile.cmake
+++ ports/instrument-script-server/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/instrument-script-server
     REF v${VERSION}
-    SHA512 bb75de7818afced7a8b992e728ec7a3060bac6cc97bdae83190d2a7c94c1902105c69406c0922a7cc4f5d0d0277242deaded47cf545824945d7fb59213330e68
+    SHA512 287008c324c0e78e7dc4d87d5c0709ec079757ff31252bfd30de0aafe10d8d96c13358df2db5028082932d0e3489d0a953a28dbd33dec0f35623165782810228
 )
 
 vcpkg_cmake_configure(
