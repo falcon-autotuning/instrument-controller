cb0f6dfcb174f3bd15a19b0e7d2347efd75a630d
Daniel Schug <daschug1@gmail.com>
2026-07-07 15:14:51 -0400

    fix: bumped iss version correctly

--- ports/instrument-script-server/portfile.cmake
+++ ports/instrument-script-server/portfile.cmake
@@ -1,22 +1,20 @@
-get_filename_component(WORKSPACE_ROOT "${CURRENT_PORT_DIR}/../../.." ABSOLUTE)
-set(LOCAL_ISS_PATH "${WORKSPACE_ROOT}/instrument-script-server")
-if(EXISTS "${LOCAL_ISS_PATH}")
-    set(SOURCE_PATH "${LOCAL_ISS_PATH}")
-else()
-    vcpkg_from_github(
-        OUT_SOURCE_PATH SOURCE_PATH
-        REPO falcon-autotuning/instrument-script-server
-        REF v${VERSION}
-        SHA512 b87cad602a3187e654ee973f0918c02c11d61b038040ea0ce2fe0124a0fd8ccfdd6918e2fd6dec152e6115e1b92c342379366b6200397fca4229fa1f45189b0a
-    )
-endif()
+vcpkg_from_github(
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO falcon-autotuning/instrument-script-server
+    REF v${VERSION}
+    SHA512 4b935148b8e0467142c8ae6b93a31035fa447021de0426113420642bc9678902de008e80aa34fb61c0e265ddbce29e6351a9c5c758afd890b42ab3eed3805db5
+    PATCHES fix-grpc-case.patch
+)
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        -DBUILD_CLI=ON
 )
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME instrument-script-server CONFIG_PATH share/instrument-script-server)
+vcpkg_copy_tools(TOOL_NAMES instrument-script-server AUTO_CLEAN)
 
 
 
