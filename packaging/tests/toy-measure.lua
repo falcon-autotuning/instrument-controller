2aa2e43f1319ad8ea4e1187088e8865fb5fc4907
tjk144 <tkovach@wisc.edu>
2026-08-25 16:00:23 -0500

    Removing the packaging folder

--- packaging/tests/toy-measure.lua
+++
@@ -1,8 +0,0 @@
--- Simple toy measurement test
-context:log("Starting toy measurement test")
-
-local resp = context:call("MockInstrument1.GET_DOUBLE")
-local val = resp:value()
-context:log(string.format("GET_DOUBLE returned: %s", tostring(val)))
-
-context:log("Toy test PASSED")
