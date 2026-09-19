8da1228d7268847a8a16786d2380d65bab912df1
tjk144 <tkovach@wisc.edu>
2026-09-18 20:10:36 -0500

    Updated the hashes for the latest VisaPlugin and ISS. Gain/ Offset support are now included in the ISS and the VisaPlugin supports an argument indicating to always read awknoledgements

--- ports/instrument-script-server/portfile.cmake
+++ ports/instrument-script-server/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/instrument-script-server
     REF v${VERSION}
-    SHA512 bee6d1fc004d555be3f178dcfd35ca6e82a95acd56b404bdd0b0e00f4bfe38e85e20ab198597bed176dbf82693a8b342eb0b5101c162017ebd706965e268ce57
+    SHA512 08de686fd32074e434d574874d5ae3484bcc664894298444e0b00a17af1392512bb527f2167da3f8266d3a29f967b81e1faddc9f1afe5e75d2e8b09feee62ca7
 )
 
 vcpkg_cmake_configure(
