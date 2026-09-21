d5f5f58ae3646e147c6f1c6810fd75ded79d379b
tjk144 <tkovach@wisc.edu>
2026-09-21 15:21:49 -0500

    Bumping to the latest ISS version without the poisoning

--- ports/instrument-script-server/portfile.cmake
+++ ports/instrument-script-server/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO falcon-autotuning/instrument-script-server
     REF v${VERSION}
-    SHA512 08de686fd32074e434d574874d5ae3484bcc664894298444e0b00a17af1392512bb527f2167da3f8266d3a29f967b81e1faddc9f1afe5e75d2e8b09feee62ca7
+    SHA512 b6681d6e550d2870b8c652dbad58b39c198547828d3318fd7976013ccb346644c076a9e1541cce15a7eb82fadf3a31a61f378163f59231cb88fcd960fdf2a3b8
 )
 
 vcpkg_cmake_configure(
